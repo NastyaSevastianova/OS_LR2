6d6dcad58fef3bddd761bd7eea465a3c3a495991
NastyaSevastianova <116763407+NastyaSevastianova@users.noreply.github.com>
2022-11-29 19:28:15 +0300

    Add files via upload

---
+++ OS_LR2_Reader/OS_LR2_Reader/OS_LR2_Reader.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include<windows.h>
+#include<string>
+
+using namespace std;
+
+void Reader()
+{
+	setlocale(LC_ALL, "Russian");
+	string FileMap; 
+	HANDLE HandleMap; 
+	LPVOID Memory; 
+	cout << "Введите имя файла: ";
+	cin >> FileMap;
+
+	HandleMap = OpenFileMappingA(FILE_MAP_READ | FILE_MAP_WRITE, FALSE, FileMap.c_str());
+	//проецирование фрагмента файла в память
+	Memory = MapViewOfFile(HandleMap, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+	if (Memory)
+	{
+		cout << "Содержимое файла " << (char*)Memory << endl << endl;
+		cout << "Для завершения введите любой символ: ";
+		cin >> FileMap;
+		UnmapViewOfFile(Memory); //завершение проецирования файла
+	}
+	else
+	{
+		cout << "Ошибка!";
+	}
+}
+
+int main()
+{
+	Reader();
+	return 0;
+}
